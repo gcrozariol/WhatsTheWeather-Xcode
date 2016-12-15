7ae692077c01ea7970f4c1474c68194a991c6055
Guilherme Crozariol <guilherme@guilhermes-macbook-pro.local>
2016-12-15 16:46:55 -0200

    Initial Commit

---
+++ What Is The Weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  What Is The Weather
+//
+//  Created by Guilherme Henrique Crozariol on 2016-12-15.
+//  Copyright © 2016 Lion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
